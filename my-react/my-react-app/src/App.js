1350e1fd36d024c4a75f47fbcc9b5670b514977f
JChaoWang <843078247@qq.com>
2021-05-05 22:36:54 +0800

    feat: 初始化项目

--- my-react/my-react-app/src/App.js
+++ my-react/my-react-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Suspense } from 'react';
-import Example from './Components/Hooks';
+// import React, { Suspense } from 'react';
+// import Example from './components/Hooks';
 
-const Welcome = React.lazy(() => import('./Components/Welcome'));
-const NumberList = React.lazy(() => import('./Components/NumberList'));
-const NameForm = React.lazy(() => import('./Components/NameForm'));
+// const Welcome = React.lazy(() => import('./components/Welcome'));
+// const NumberList = React.lazy(() => import('./components/NumberList'));
+// const NameForm = React.lazy(() => import('./components/NameForm'));
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <header>
+//           <Welcome name="WangJiChao" />
+//           <NumberList />
+//           <NameForm />
+//           <Example />
+//         </header>
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Welcome name="WangJiChao" />
-          <NumberList />
-          <NameForm />
-          <Example />
-        </header>
-      </Suspense>
+      <h1>This is React App.</h1>
     </div>
   );
 }

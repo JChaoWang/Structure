525aacac0f38fb3d692738704b0c3fca707afa01
jc_Wang <843078247@qq.com>
2021-05-25 09:49:56 +0800

    feat: delete

--- Typescript/types.js
+++
@@ -1,9 +0,0 @@
-// let isDone: boolean = false
-// let decLiteral: number = 20 // 10进制
-// let hexLiteral: number = 0x14 // 16进制
-// let binaryLiteral: number = 0b10100 // 2进制
-// let octalLiteral: number = 0o24 // 8进制
-var myName = 'wade';
-var age = 29;
-var sentence = "Hello, my name is " + myName + ". I'm " + age + " years old.";
-console.log(sentence);
